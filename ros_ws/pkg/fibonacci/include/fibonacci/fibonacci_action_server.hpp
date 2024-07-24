d0deff8e4de00bb8a5b3933b0072198e99f74933
Riliumph <riliumph@outlook.jp>
2024-07-24 18:12:20 +0000

    [add] フィボナッチサーバーライブラリを追加

--- ros_ws/pkg/fibonacci/include/fibonacci/fibonacci_action_server.hpp
+++ ros_ws/pkg/fibonacci/include/fibonacci/fibonacci_action_server.hpp
@@ -1,19 +1,38 @@
 #ifndef FIBONACCI__FIBONACCI_ACTION_SERVER_HPP_
 #define FIBONACCI__FIBONACCI_ACTION_SERVER_HPP_
-
+// STL
+#include <functional>
+#include <memory>
+#include <thread>
+// ROS2
+#include "rclcpp/rclcpp.hpp"
+#include "rclcpp_action/rclcpp_action.hpp"
+// ROS2 automatically created definition
 #include "fibonacci/visibility_control.h"
+#include "fibonacci_msg/action/fibonacci.hpp"
 
-namespace fibonacci
-{
+namespace fibonacci {
 
-class FibonacciActionServer
+class FibonacciActionServer : public rclcpp::Node
 {
+  using ActMsg = fibonacci_msg::action::Fibonacci;
+  using GoalHandle = rclcpp_action::ServerGoalHandle<ActMsg>;
+
 public:
-  FibonacciActionServer();
+  static const char* node_name;
+  static const char* server_name;
+
+  FIBONACCI_PUBLIC explicit FibonacciActionServer(
+    const rclcpp::NodeOptions& options);
 
   virtual ~FibonacciActionServer();
+
+private:
+  rclcpp_action::Server<ActMsg>::SharedPtr action_server_;
+
+  void execute(const std::shared_ptr<GoalHandle> goal_handle);
 };
 
-}  // namespace fibonacci
+} // namespace fibonacci
 
-#endif  // FIBONACCI__FIBONACCI_ACTION_SERVER_HPP_
+#endif // FIBONACCI__FIBONACCI_ACTION_SERVER_HPP_
--- ros_ws/pkg/fibonacci/src/fibonacci_action_server.cpp
+++ ros_ws/pkg/fibonacci/src/fibonacci_action_server.cpp
@@ -1,14 +1,80 @@
 #include "fibonacci/fibonacci_action_server.hpp"
 
-namespace fibonacci
-{
+namespace fibonacci {
+const char* node_name = "fibonacci_action_server_node";
+const char* server_name = "fibonacci_action_server";
 
-FibonacciActionServer::FibonacciActionServer()
+/// @brief コンストラクタ
+/// @param options ROS2のノード設定
+FibonacciActionServer::FibonacciActionServer(
+  const rclcpp::NodeOptions& options = rclcpp::NodeOptions())
+  : Node(node_name, options)
 {
+  // using namespace std::placeholders;
+  auto goal_handler = [this](const rclcpp_action::GoalUUID& uuid,
+                             std::shared_ptr<const ActMsg::Goal> goal) {
+    RCLCPP_INFO(
+      get_logger(), "Received goal request with order %d", goal->order);
+    (void)uuid;
+    return rclcpp_action::GoalResponse::ACCEPT_AND_EXECUTE;
+  };
+
+  auto cancel_handler = [this](const std::shared_ptr<GoalHandle> goal_handle) {
+    RCLCPP_INFO(get_logger(), "Received request to cancel goal");
+    (void)goal_handle;
+    return rclcpp_action::CancelResponse::ACCEPT;
+  };
+
+  auto handle_accepted = [this](const std::shared_ptr<GoalHandle> goal_handle) {
+    // エグゼキュータをブロックしないように迅速に返す必要があるので、
+    // 新しいスレッド内で呼び出されるラムダ関数を宣言します
+    auto execute_in_thread = [this, goal_handle]() {
+      return execute(goal_handle);
+    };
+    std::thread{ execute_in_thread }.detach();
+  };
+
+  action_server_ = rclcpp_action::create_server<ActMsg>(
+    this, server_name, goal_handler, cancel_handler, handle_accepted);
 }
 
-FibonacciActionServer::~FibonacciActionServer()
+/// @brief 実行関数
+/// @param goal_handle
+void
+FibonacciActionServer::execute(const std::shared_ptr<GoalHandle> goal_handle)
 {
-}
+  RCLCPP_INFO(get_logger(), "Executing goal");
+  rclcpp::Rate loop_rate(1);
+  const auto goal = goal_handle->get_goal();
+  auto feedback = std::make_shared<ActMsg::Feedback>();
+  auto& sequence = feedback->partial_sequence;
+  sequence.push_back(0);
+  sequence.push_back(1);
+  auto result = std::make_shared<ActMsg::Result>();
 
-}  // namespace fibonacci
+  for (int i = 1; (i < goal->order) && rclcpp::ok(); ++i) {
+    // Check if there is a cancel request
+    if (goal_handle->is_canceling()) {
+      result->sequence = sequence;
+      goal_handle->canceled(result);
+      RCLCPP_INFO(get_logger(), "Goal canceled");
+      return;
+    }
+    // Update sequence
+    sequence.push_back(sequence[i] + sequence[i - 1]);
+    // Publish feedback
+    goal_handle->publish_feedback(feedback);
+    RCLCPP_INFO(get_logger(), "Publish feedback");
+
+    loop_rate.sleep();
+  }
+
+  // Check if goal is done
+  if (rclcpp::ok()) {
+    result->sequence = sequence;
+    goal_handle->succeed(result);
+    RCLCPP_INFO(get_logger(), "Goal succeeded");
+  }
+};
+
+} // namespace fibonacci
